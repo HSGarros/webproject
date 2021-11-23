dc1b1d4a3ed814aa41670ad1caa9a511de8fc992
“HSGarros” <1969541342@qq.com>
2021-11-23 23:23:52 +0800

    new code

--- src/main/java/com/controller/LoginServlet.java
+++ src/main/java/com/controller/LoginServlet.java
@@ -15,11 +15,12 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
         req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req,resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String account = req.getParameter("account");
         String password = req.getParameter("pwd");
         String url;
@@ -28,8 +29,12 @@ public class LoginServlet extends HttpServlet {
             req.getSession().setAttribute("accounter", accounter);
             url = "/admin";
         }else if ("123".equals(account) && "123".equals(password)) {
+            Accounter accounter = new Accounter("123",0);
+            req.getSession().setAttribute("accounter", accounter);
             url = "/welcome";
         } else if (AccounterServiceImpl.comparePassward(account,password)) {
+            Accounter accounter = new Accounter("123",0);
+            req.getSession().setAttribute("accounter", accounter);
             url = "/welcome";
         } else {
             url = "/login";
